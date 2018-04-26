a7310dcc8f762cb6532527e49c298eb2e873793a
Liang Niu <niuliang3@gmail.com>
2018-04-26 13:57:17 -0400

    customize deploy commands

--- deploy.sh
+++ deploy.sh
@@ -2,25 +2,32 @@
 # stack exec myblog watch
 
 # Temporarily store uncommited changes
-git stash
+# git stash
 
 # Verify correct branch
-git checkout develop
+# git checkout develop
 
 # Build new files
-stack exec myblog clean
-stack exec myblog build
+# stack exec myblog clean
+# stack exec myblog build
+stack exec myblog rebuild
 
 # Get previous files
 git fetch --all
 git checkout -b master --track origin/master
 
 # Overwrite existing files with new files
-cp -a _site/. .
+rsync -a --filter='P _site/'      \
+         --filter='P _cache/'     \
+         --filter='P .git/'       \
+         --filter='P .gitignore'  \
+         --filter='P .stack-work' \
+         --delete-excluded        \
+         _site/ .
 
 # Commit
 git add -A
-git commit -m "Publish."
+git commit -m ""
 
 # Push
 git push origin master:master
@@ -28,5 +35,5 @@ git push origin master:master
 # Restoration
 git checkout develop
 git branch -D master
-git stash pop
+#git stash pop
 
